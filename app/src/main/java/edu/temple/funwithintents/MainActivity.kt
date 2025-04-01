c39635074cc56b53602b4695474194a0daf605c4
jslootsky <joshuaslootsky@gmail.com>
2025-04-01 11:56:08 -0400

    Created the send text functionality which copies the text box to a text messaging app

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -1,8 +1,10 @@
 package edu.temple.funwithintents
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -15,7 +17,15 @@ class MainActivity : AppCompatActivity() {
 
         // When the user clicks this button, share the text if not empty
         findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
-
+            if(editText.text == null){
+                Toast.makeText(this, "Empty Text", Toast.LENGTH_LONG).show()
+            }else {
+                val intent = Intent(Intent.ACTION_SEND).apply {
+                    type = "text/plain"
+                    putExtra(Intent.EXTRA_TEXT, editText.text.toString())
+                }
+                startActivity(Intent.createChooser(intent, "Share via"))
+            }
         }
     }
 }
